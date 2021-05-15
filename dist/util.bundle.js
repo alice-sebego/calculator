13430e0197e1a07de80ecd2879de5d712f1bc8f9
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-05-16 00:34:09 +0200

    Install Babel

--- dist/util.bundle.js
+++ dist/util.bundle.js
@@ -16,7 +16,7 @@
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lodash */ \"./node_modules/lodash/lodash.js\");\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_0__);\n\r\n// Handle year's display\r\nconst $year = document.querySelector(\"#year\");\r\nconst date = new Date(Date.now());\r\nconst year = date.getFullYear();\r\n\r\n$year.textContent = year;\n\n//# sourceURL=webpack://calculator/./js/util.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lodash */ \"./node_modules/lodash/lodash.js\");\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_0__);\n // Handle year's display\n\nvar $year = document.querySelector(\"#year\");\nvar date = new Date(Date.now());\nvar year = date.getFullYear();\n$year.textContent = year;\n\n//# sourceURL=webpack://calculator/./js/util.js?");
 
 /***/ }),
 
