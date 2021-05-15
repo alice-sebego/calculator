ba4740fb5aa6e62376b7bd4ce4d5a316bcd75469
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-05-15 21:07:00 +0200

    Rewrite variables of pythagore.js in ES6

--- js/pythagore.js
+++ js/pythagore.js
@@ -3,23 +3,20 @@
  * @param {number} a 
  * @param {number} b 
  */
+const lengthHypotenuse = (a, b) =>  Math.hypot(a, b);
 
-const lengthHypotenuse = (a, b) => {
-    return Math.hypot(a, b);
-}
-
-const $calcHypotenuse = document.getElementById('calcHypotenuse');
+const $calcHypotenuse = document.querySelector('#calcHypotenuse');
 
 $calcHypotenuse.addEventListener('click', e => {
     
     e.preventDefault();
     
-    const aCote = document.getElementById('a').value;
-    const bCote = document.getElementById('b').value;
+    const aCote = document.querySelector('#a').value;
+    const bCote = document.querySelector('#b').value;
     
     lengthHypotenuse(aCote,bCote);
     
-    const $resultHypothenuse = document.getElementById('result-hypothenuse');
+    const $resultHypothenuse = document.querySelector('#result-hypothenuse');
     $resultHypothenuse.textContent = lengthHypotenuse(aCote,bCote) + ' cm';
     $resultHypothenuse.style.border = "1px dashed rgb(158, 201, 204)";
 });
@@ -29,23 +26,20 @@ $calcHypotenuse.addEventListener('click', e => {
  * @param {number} s 
  * @param {number} c 
  */
+const lenghtOtherSegment = (s, c) => Math.sqrt((c * c) - (s * s));
 
-const lenghtOtherSegment = (s, c) => {
-    return Math.sqrt((c * c) - (s * s));
-}
-
-const $calcSegment = document.getElementById('calcSegment');
+const $calcSegment = document.querySelector('#calcSegment');
 
 $calcSegment.addEventListener('click', e => {
     
     e.preventDefault();
     
-    const hypotenuse = document.getElementById('c').value;
-    const cote = document.getElementById('s').value;
+    const hypotenuse = document.querySelector('#c').value;
+    const cote = document.querySelector('#s').value;
     
     lenghtOtherSegment(cote, hypotenuse);
     
-    const $resultSegment = document.getElementById('result-segment');
+    const $resultSegment = document.querySelector('#result-segment');
     $resultSegment.textContent = lenghtOtherSegment(cote, hypotenuse) + ' cm';
     $resultSegment.style.border = "1px dashed rgb(158, 201, 204)";
 });
