e49f5f3907d433782328031c3705c80f044a7fba
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-05-15 21:01:04 +0200

    Rewrite functions of pythagore.js

--- js/pythagore.js
+++ js/pythagore.js
@@ -4,21 +4,25 @@
  * @param {number} b 
  */
 
-function lengthHypotenuse(a, b){
+const lengthHypotenuse = (a, b) => {
     return Math.hypot(a, b);
 }
 
 const $calcHypotenuse = document.getElementById('calcHypotenuse');
 
-$calcHypotenuse.addEventListener('click', function(e){
+$calcHypotenuse.addEventListener('click', e => {
+    
     e.preventDefault();
+    
     const aCote = document.getElementById('a').value;
     const bCote = document.getElementById('b').value;
+    
     lengthHypotenuse(aCote,bCote);
+    
     const $resultHypothenuse = document.getElementById('result-hypothenuse');
     $resultHypothenuse.textContent = lengthHypotenuse(aCote,bCote) + ' cm';
     $resultHypothenuse.style.border = "1px dashed rgb(158, 201, 204)";
-})
+});
 
 /**
  * Calculate the other segments of triangle
@@ -26,18 +30,22 @@ $calcHypotenuse.addEventListener('click', function(e){
  * @param {number} c 
  */
 
-function lenghtOtherSegment(s, c){
+const lenghtOtherSegment = (s, c) => {
     return Math.sqrt((c * c) - (s * s));
 }
 
 const $calcSegment = document.getElementById('calcSegment');
 
-$calcSegment.addEventListener('click', function(e){
+$calcSegment.addEventListener('click', e => {
+    
     e.preventDefault();
+    
     const hypotenuse = document.getElementById('c').value;
     const cote = document.getElementById('s').value;
+    
     lenghtOtherSegment(cote, hypotenuse);
+    
     const $resultSegment = document.getElementById('result-segment');
     $resultSegment.textContent = lenghtOtherSegment(cote, hypotenuse) + ' cm';
     $resultSegment.style.border = "1px dashed rgb(158, 201, 204)";
-})
+});
